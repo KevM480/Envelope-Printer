8fe70318781e263196ef1c8b7462bd846e777958
Kevin <kmilligan480@gmail.com>
2024-04-28 08:49:33 -0400

    Config creates new font obj when properties change

--- src/main/java/com/kevinm/envelopeprinter/properties/EnvelopePrinterConfig.java
+++ src/main/java/com/kevinm/envelopeprinter/properties/EnvelopePrinterConfig.java
@@ -39,7 +39,8 @@ public class EnvelopePrinterConfig implements ConfigProperties {
 
 	@Override
 	public void afterSavingProperties() {
-
+		senderFont = new Font(senderFontName, Font.PLAIN, senderFontSize);
+		recieverFont = new Font(receiverFontName, Font.PLAIN, receiverFontSize);
 	}
 
 }
